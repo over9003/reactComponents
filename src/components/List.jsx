db5dcfc3e5d48cc9f4db5a118dfae2838ead9cf1
over9003 <gabrielvartolomeu@yahoo.com>
2021-04-28 01:06:36 +0300

    Initial commit

---
+++ src/components/List.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+      <li>SM</li>
+      <li>Pasta</li>
+    </ul>
+  );
+}
+
+export default List;
